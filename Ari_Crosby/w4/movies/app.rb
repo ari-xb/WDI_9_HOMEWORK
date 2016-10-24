4c5b460344d5b8420a7898bca2b892b029d6c5e2
ari-xb <rexb83@gmail.com>
2016-10-24 14:44:55 +1100

    Fri HW

--- Ari_Crosby/w4/movies/app.rb
+++ Ari_Crosby/w4/movies/app.rb
@@ -1,3 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
+require 'pry'
+
+get '/index' do
+  erb :index
+end
+
+get '/about' do
+  flick_search = params[:flicksearch].to_s
+  #num2 = params[:num2].to_f
+  @flick = HTTParty.get("http://omdbapi.com/?t=#{flick_search}")
+  # @flick.each do |k, v|
+  #   puts
+  # end
+  @title = @flick["Title"]
+  @plot = @flick["Plot"]
+  @poster = @flick["Poster"]
+  # @title = @flick["Title"]
+  # @title = @flick["Title"]
+  # @title = @flick["Title"]
+  # @title = @flick["Title"]
+
+  erb :about
+end
